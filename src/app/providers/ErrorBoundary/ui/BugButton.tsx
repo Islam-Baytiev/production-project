73336e3af41eb06c579e1dff2f2475d6872c36ef
Islam Baytiev <topmuradov@gmail.com>
2023-01-14 18:57:43 +0300

    добавил errorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+
+// Для тест еррорбаундри
+export const BugButton = () => {
+  const { t } = useTranslation();
+  const [error, setError] = useState(false);
+  const onThrow = () => setError(true);
+
+  useEffect(() => {
+    if (error) throw new Error();
+  }, [error]);
+  return (
+    <Button
+      onClick={onThrow}
+    >
+      {t('throwError')}
+    </Button>
+  );
+};
--- src/widgets/LangSwitcher/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/LangSwitcher.tsx
@@ -19,7 +19,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
       theme={ThemeButton.CLEAR}
       onClick={toogle}
     >
-      {t('Язык')}
+      {t('Русский')}
     </Button>
 
   );
