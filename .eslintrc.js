d49a72b36c6eb5d757b2f951f1a82c331b0dec3d
Islam Baytiev <topmuradov@gmail.com>
2023-01-18 00:48:44 +0300

    подключил сторибук

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,7 +41,7 @@ module.exports = {
     'i18next/no-literal-string': ['error',
       {
         markupOnly: true,
-        ignoreAtttibute: ['data=testid'],
+        ignoreAtttibute: ['data=testid', 'to'],
       },
     ],
     'max-len': ['error', { ignoreComments: true, code: 110 }],
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,5 +1,5 @@
 import {
-  fireEvent, getByTestId, render, screen,
+  fireEvent, screen,
 } from '@testing-library/react';
 import { renderWithTranslation }
   from 'shared/lib/tests/renderWithTranslation/renderWithTranslation';
