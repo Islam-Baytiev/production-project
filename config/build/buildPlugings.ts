76b2085e6a558985a2033e28a50657c9c04b42b2
Islam Baytiev <islam.baytiev@pirs.online>
2023-04-09 07:37:14 +0300

    подключение лоадера рефреш

--- config/build/buildPlugings.ts
+++ config/build/buildPlugings.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -24,6 +25,7 @@ export function buildPlugins({
   ];
 
   if (isDev) {
+    plugin.push(new ReactRefreshWebpackPlugin());
     plugin.push(new webpack.HotModuleReplacementPlugin());
     plugin.push(new BundleAnalyzerPlugin({
       openAnalyzer: false,
